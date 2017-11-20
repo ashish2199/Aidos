1a03303f2f938977077c4e903597f5237f588e86
Corey Lovett-Hendrey <coreyhendrey@gmail.com>
2017-11-20 10:27:29 +1000

    Added more verbose comments to Renderer to further explain its functionality. Added a function to explain itself a little better. Made playSpeed work properly. (It's actually set now)

--- src/bomberman/Renderer.java
+++ src/bomberman/Renderer.java
@@ -11,34 +11,48 @@ public class Renderer
 	/**
 	 *
 	 * @author CoreyHendrey
+	 *
+	 * gc is GraphicsContext on which to draw the sprite
+	 * time is the current time to sync up too, ie the time from the gameloop
+	 *
+	 * This works by first finding the top left pixel of the animation that is playing, with spriteLocationOnSheet(X/Y).
+	 * It then figures out which from we are up to using findCurrentFrame().
+	 *
+	 * It then draws the frame at x, y using the height and width.
+	 *
+	 * It uses 'actualSize' to find the next sprite, because of padding on the width and height.
+	 *
 	 */
-    static Image img;
+
+   static Image img;
 
     public static void init() {
         img = ImageUtils.loadImage("src/Resources/img/sprites.png");
     }
 
-    // "ActualSize" refers to how many pixels across to look for the next sprite
-    
     public static void playAnimation(Sprite sprite) {
         double time =  Sandbox.getTickDuration();
         GraphicsContext gc = Sandbox.getGraphicsContext();
-        playAnimation(gc, time, sprite.actualSize, sprite.spriteLocationOnSheetX, sprite.spriteLocationOnSheetY, sprite.numberOfFrames, sprite.x, sprite.y, sprite.width, sprite.height, sprite.scale, sprite.resersePlay);
+        playAnimation(gc, time, sprite.actualSize, sprite.spriteLocationOnSheetX, sprite.spriteLocationOnSheetY, sprite.numberOfFrames, sprite.x, sprite.y, sprite.width, sprite.height, sprite.scale, sprite.resersePlay, sprite.playSpeed);
     }
 
-    public static void playAnimation(GraphicsContext gc, double time, int actualSize, int startingPointX, int startingPointY, int numberOfFrames, int x, int y, int width, int height, boolean leftToRight) {
-    	playAnimation(gc, time, actualSize, startingPointX, startingPointY, numberOfFrames, x, y, width, actualSize, 1, leftToRight);
+    public static void playAnimation(GraphicsContext gc, double time, int actualSize, int startingPointX, int startingPointY, int numberOfFrames, int x, int y, int width, int height, boolean leftToRight, double playSpeed) {
+    	playAnimation(gc, time, actualSize, startingPointX, startingPointY, numberOfFrames, x, y, width, actualSize, 1, leftToRight, playSpeed);
     }
 
-    public static void playAnimation(GraphicsContext gc, double time, int actualSize, int startingPointX, int startingPointY, int numberOfFrames, int x, int y, int width, int height, double scale, boolean reversePlay) {
-    	double duration = 0.3;
-    	int index = (int)(time % (numberOfFrames * duration) / duration);
+    public static void playAnimation(GraphicsContext gc, double time, int actualSize, int startingPointX, int startingPointY, int numberOfFrames, int x, int y, int width, int height, double scale, boolean reversePlay, double playSpeed) {
+    	double speed = playSpeed >= 0 ? playSpeed : 0.3;
+    	int index = findCurrentFrame(time, numberOfFrames, speed);
 
 		int newX = reversePlay ? startingPointX + index * actualSize : startingPointX;
 		int newY = reversePlay ? startingPointY : startingPointY + index * actualSize;
-        
+
                 System.out.println(index+" "+newX+" "+newY+" "+width+" "+height+" "+x+" "+y+" "+width * scale+" "+height * scale);
-    
+
 		gc.drawImage(img, newX, newY, width, height, x, y, width * scale, height * scale);
+    }
+
+    private static int findCurrentFrame(double time, int totalFrames, double speed) {
+    	return (int)(time % (totalFrames * speed) / speed);
     }
 }
