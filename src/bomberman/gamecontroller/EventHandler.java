aeaccde8be87289642eb5e184a15971f753f6285
Corey Lovett-Hendrey <coreyhendrey@gmail.com>
2017-11-23 11:48:34 +1000

    Made the EventHandler independant of Player

--- src/bomberman/gamecontroller/EventHandler.java
+++ src/bomberman/gamecontroller/EventHandler.java
@@ -5,13 +5,7 @@
  */
 package bomberman.gamecontroller;
 
-import bomberman.GameLoop;
-import bomberman.constants.Direction;
-import bomberman.entity.player.Player;
-import bomberman.scenes.Sandbox;
-import com.sun.javafx.sg.prism.NGCanvas;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 /**
@@ -21,47 +15,38 @@ import javafx.scene.input.KeyEvent;
 public class EventHandler {
     public static char lastKeyPress;
     public static char lastKeyReleased;
-    
+
     public static void attachEventHandlers(Scene s){
-        keyReleaseHanlder krh = new keyReleaseHanlder(GameLoop.player);
-        keyPressedHandler kph = new keyPressedHandler(GameLoop.player);
+        keyReleaseHanlder krh = new keyReleaseHanlder();
+        keyPressedHandler kph = new keyPressedHandler();
         s.setOnKeyReleased(krh);
         s.setOnKeyPressed(kph);
     }
 }
 class keyReleaseHanlder implements javafx.event.EventHandler<KeyEvent>{
-    Player player; 
 
-    public keyReleaseHanlder(Player p) {
-        this.player=p;
+    public keyReleaseHanlder() {
     }
     @Override
     public void handle(KeyEvent evt) {
-        player.move(0,Direction.LEFT);
         System.out.println("The key released is : "+evt.getText()+" with keycode "+evt.getCode().getName());
+
+        String code = evt.getCode().toString();
+
+        if ( InputHandler.input.contains(code) )
+        	InputHandler.input.remove( code );
     }
 }
 class keyPressedHandler implements javafx.event.EventHandler<KeyEvent>{
-    Player player; 
-
-    public keyPressedHandler(Player p) {
-        this.player=p;
-    }
-    
     @Override
     public void handle(KeyEvent evt) {
         System.out.println("The key pressed is : "+evt.getText()+" with keycode "+evt.getCode().getName());
-        if(evt.getCode()==KeyCode.UP){
-            player.move(5,Direction.UP);
-        }
-        if(evt.getCode()==KeyCode.DOWN){
-            player.move(5,Direction.DOWN);
-        }
-        if(evt.getCode()==KeyCode.LEFT){
-            player.move(5,Direction.LEFT);
-        }
-        if(evt.getCode()==KeyCode.RIGHT){
-            player.move(5,Direction.RIGHT);
-        }
+
+        String code = evt.getCode().toString();
+
+        //https://gamedevelopment.tutsplus.com/tutorials/introduction-to-javafx-for-game-development--cms-23835
+        // only add once... prevent duplicates
+        if ( !InputHandler.input.contains(code) )
+        	InputHandler.input.add( code );
     }
 }
---
+++ src/bomberman/gamecontroller/InputHandler.java
@@ -0,0 +1,8 @@
+package bomberman.gamecontroller;
+
+import java.util.ArrayList;
+
+public class InputHandler
+{
+	public static ArrayList<String> input = new ArrayList<String>();
+}
