199b2a2dace857e715bf709678f39ece04a23884
limqiying <limqiying19@gmail.com>
2018-04-09 14:14:16 -0400

    consolidated commit

--- Aidos-master/src/bomberman/animations/Animations.java
+++
@@ -1,7 +0,0 @@
-package bomberman.animations;
-
-import bomberman.animations.sprites.Sprite;
-
-public interface Animations {
-	public Sprite getSprite();
-}
--- Aidos-master/src/bomberman/entity/movements/MovementStrategy.java
+++
@@ -1,7 +0,0 @@
-package bomberman.entity.movements;
-
-import bomberman.constants.Direction;
-
-public interface MovementStrategy {
-	Direction getDirection();
-}
