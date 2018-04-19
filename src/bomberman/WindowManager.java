f84a1e887930ecbd661d9949ce12ea0e78117902
tialim <limqiying19@gmail.com>
2018-04-19 18:51:24 -0400

    add level adjust buttons

--- src/bomberman/WindowManager.java
+++ src/bomberman/WindowManager.java
@@ -11,6 +11,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Separator;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToolBar;
@@ -19,7 +23,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
 public class WindowManager {
-	
+
 	GameHandler gameHandler;
 	Scene s;
 	BorderPane b = new BorderPane();
@@ -30,8 +34,8 @@ public class WindowManager {
 	public WindowManager(GameHandler gh) {
 		gameHandler = gh;
 		s = new Scene(b, GlobalConstants.BACKGROUND_COLOR);
-		b.setTop(createToolBar()); 		
-		b.setBackground(Background.EMPTY);	// for scene background to show through
+		b.setTop(createToolBar());
+		b.setBackground(Background.EMPTY); // for scene background to show through
 		GameEventHandler.attachEventHandlers(s);
 	}
 
@@ -54,37 +58,53 @@ public class WindowManager {
 		System.out.println(s.getWidth());
 		System.out.println(s.getHeight());
 	}
-	
-	private HBox createMenuBar() {
+
+	private HBox createControlBar() {
 		// -------- create menu items --------
 		Button newGame = new Button("New Game");
 		newGame.setFocusTraversable(false);
 		newGame.setOnAction(event -> gameHandler.newGame());
-		
+
 		ToggleButton pauseGame = new ToggleButton("Pause/Play");
 		pauseGame.setFocusTraversable(false);
 		pauseGame.setOnAction(event -> {
-		    if (pauseGame.isSelected()) {
-		    	gameHandler.stopGame();
-		    }else {
-		    	gameHandler.resumeGame();
-		    }
-		    state.setValue("Game Status: " + GameState.gameStatus);
+			if (pauseGame.isSelected()) {
+				gameHandler.stopGame();
+			} else {
+				gameHandler.resumeGame();
+			}
+			state.setValue("Game Status: " + GameState.gameStatus);
 		});
-		
-		return new HBox(newGame, pauseGame);
+
+		return new HBox(newGame, pauseGame, createLevelMenu());
 	}
 
 	private ToolBar createToolBar() {
 		HBox hbox = new HBox(20);
 		Label levelStats = new Label("Level: 0");
 		Label gameState = new Label("gameState: -");
-		levelStats.textProperty().bind(level);
-		gameState.textProperty().bind(state);
+		levelStats.textProperty()
+				.bind(level);
+		gameState.textProperty()
+				.bind(state);
 		hbox.setAlignment(Pos.CENTER_LEFT);
-		hbox.getChildren().addAll(createMenuBar(), levelStats, new Separator(Orientation.VERTICAL), gameState);
+		hbox.getChildren()
+				.addAll(createControlBar(), levelStats, new Separator(Orientation.VERTICAL), gameState);
 		ToolBar toolBar = new ToolBar(hbox);
 		return toolBar;
+	}
+	
+	// used for development
+	private MenuButton createLevelMenu() {
+		MenuButton levelSwitch = new MenuButton("Change Level");
+		
+		// level 2 button
+		MenuItem level2 = new MenuItem("Level 2");
+		level2.setOnAction(event -> gameHandler.loadLevel(2));
+		
+		levelSwitch.getItems().add(level2);
+		levelSwitch.setFocusTraversable(false);
+		return levelSwitch;
 	}
 
 }
